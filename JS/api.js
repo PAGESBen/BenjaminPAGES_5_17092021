e6f8e2c6b0c96e893e49e05966e320a9b14588d4
Benjamin PAGES <b.pages@yahoo.fr>
2021-09-26 12:22:26 +0200

    finalisation de la fonction run()

--- JS/api.js
+++ JS/api.js
@@ -5,27 +5,64 @@ async function run() {
     let ours = await get.json()
     for (let peluche of ours) {
 
-// variable let pour rechercher l'element conteneur dans le dom -- let elt = document.getElementById('main');
-        let mainContainer = document.getElementById('productslist')
+        console.log(peluche)
         
-//generation les elements du dom -- const newElt = document.createElement("div");
-        const col = document.createElement("div.col.col-lg-4")
-        const card = document.createElement("div.card.m-2")
-        const cardBody = document.createElement("div.card-body")
-        const name = document.createElement("h2.card-title.text-center")
-        const description = document.createElement("p.card-tect")
-        const button = document.createElement("a.btn.btn-primary.stretched-link.m-2")
+        //generation les elements du dom -- const newElt = document.createElement("div") + ajout de leur classes nomElement.classList.add("", "", "", "");
 
-//placer les variables dans les elements -- elt.innerHTML = "code + variable";
-        card.innerHTML = "<img class=”card-img-top” src="${peluche.image}" alt=”...”>"
+        const col = document.createElement("div")
+        // .col.col-lg-4
+        col.classList.add("col-12","col-lg-4")
+        
+        const card = document.createElement("div")
+        // .card.m-2
+        card.classList.add("card","m-2")
 
-//imbriquer et generer les elements dans le DOM -- elt.appendChild(newElt);
+        const productImg = document.createElement("img")
+        // .card-img-top
+        productImg.classList.add("card-img-top")
+
+        const cardBody = document.createElement("div")
+        // .card-body
+        cardBody.classList.add("card-body")
+
+        const name = document.createElement("h2")
+        // .card-title.text-center
+        name.classList.add("card-title","text-center")
+        
+        const description = document.createElement("p")
+        // .card-text
+        description.classList.add("card-text")
+
+        const price = document.createElement("p")
+        // .card-text
+        price.classList.add("card-text")
+
+        const button = document.createElement("a")
+        // .btn.btn-primary.stretched-link.m-2
+        button.classList.add("btn", "btn-primary", "stretched-link", "m-2")
+
+
+        //placer les variables dans les elements -- elt.innerHTML = "code + variable";
+
+        productImg.setAttribute("src", peluche.imageUrl)
+        name.textContent = peluche.name
+        description.textContent = peluche.description
+        price.textContent = "Prix : " + peluche.price + " €"
+        button.textContent = "Voir l'article"
+
+
+        // On va récuperer le contener principal dans le DOM -- let elt = document.getElementById('main');
+        let mainContainer = document.getElementById('productslist')
+
+        //imbriquer et generer les elements dans le DOM -- elt.appendChild(newElt);
         mainContainer.appendChild(col)
         col.appendChild(card)
+        card.appendChild(productImg)
         card.appendChild(cardBody)
         card.appendChild(button)
         cardBody.appendChild(name)
         cardBody.appendChild(description)
+        cardBody.appendChild(price)
     }
 }
 
@@ -33,13 +70,18 @@ async function run() {
 //Execution de la fonction run 
 run()
 
+/* 
+1. faire la requette au web service et recuperer les JSON 
+    -fetch 
+    -get.json
 
+2. Generer les elements des cartes et leur ajouter leur classes bootstrap : 
+    -document.createElement()
+    -nomElement.classList.add("", "", "", "")
 
-/*
-1. Créer les elements
-    let nouvelElement = document.createElement("div")
-2. leur ajouter le contenu
-    let contenu = nouvelElement.innerHTML(variable)
-3. Les placer dans le Dom
+3. leur donner un contenu : 
+    -
 
+4. Les placer dans le DOM : 
+    -nomElementParent.appendChild(elementEnfant)
 */
